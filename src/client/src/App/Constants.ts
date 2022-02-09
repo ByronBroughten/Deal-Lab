fc7b9f44f987b10cbca9bed26fbc6c185c71f116
Byron Broughten <byron.broughten@gmail.com>
2022-02-09 15:00:02 -0600

    Reverts Constants to when post requests worked

--- src/client/src/App/Constants.ts
+++ src/client/src/App/Constants.ts
@@ -3,38 +3,11 @@ import { extendUrl } from "./utils/url";
 const dev = {
   name: "Analyzer Client — Development",
   endpoint: "http://localhost:5000/api/",
-} as const;
-
-const appUrl = "https://ultimate-property-analyzer.herokuapp.com";
-const serverUrl = extendUrl(appUrl, "api");
-
-const serverRoutes = {
-  main: {
-    path: "/api",
-  },
-  get user() {
-    return {
-      path: extendUrl(this.main.path, "user"),
-    };
-  },
-  get section() {
-    return {
-      path: extendUrl(this.main.path, "section"),
-      get arr() {
-        return {
-          path: extendUrl(serverRoutes.section.path, "arr"),
-        };
-      },
-    };
-  },
-  // get tableColumns(){
-  //   return extendUrl(this.sectionArr, "/tableColumns")
-  // },
 };
 
 const prod = {
   name: "Analyzer Client — Production",
-  endpoint: "https://ultimate-property-analyzer.herokuapp.com/api",
+  endpoint: "https://ultimate-property-analyzer.herokuapp.com/api/",
 };
 
 // index should have utils, constants,
