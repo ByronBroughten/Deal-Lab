94a10b0fefe69db8353d2e72c80704d2fe615dab
Byron Broughten <byron.broughten@gmail.com>
2022-04-07 23:14:44 -0500

    Safe changes from revert

---
+++ src/client/src/App/sharedWithServer/Analyzer/FeSectionPack.ts
@@ -0,0 +1,153 @@
+import { extend, omit } from "lodash";
+import { Obj } from "../utils/Obj";
+import { FeSelfOrDescendantParentStub } from "./FeSectionPacks/FeParentStub";
+import {
+  FeSelfOrDescendantNode,
+  OneSectionNodeMaker,
+  SectionNodeMaker,
+} from "./FeSectionPacks/FeSectionNode";
+import { RawSectionPack } from "./RawSectionPack";
+import { Inf } from "./SectionMetas/Info";
+import {
+  NameToNameWithSameChildren,
+  SelfOrDescendantIds,
+} from "./SectionMetas/relNameArrs/ChildTypes";
+import { FeParentInfo } from "./SectionMetas/relNameArrs/ParentTypes";
+import { Id } from "./SectionMetas/relSections/baseSections/id";
+import { SectionName } from "./SectionMetas/SectionName";
+import {
+  GeneralRawSection,
+  RawSection,
+  RawSections,
+} from "./SectionPacks/RawSection";
+import { RawSectionFinder } from "./SectionPacks/RawSectionFinder";
+export type OrderedSectionNodeProps<SN extends SectionName> = {
+  parentFinder: FeParentInfo<SN>;
+  feId?: string;
+  idx?: number;
+};
+
+type FeSectionPackCore<SN extends SectionName> = RawSectionPack<SN, "fe">;
+export class FeSectionPack<SN extends SectionName> {
+  constructor(readonly core: FeSectionPackCore<SN>) {}
+  get sectionName(): SN {
+    return this.core.sectionName;
+  }
+  get dbId(): string {
+    return this.core.dbId;
+  }
+  get rawSections(): RawSections<SN, "fe"> {
+    return this.core.rawSections;
+  }
+  get headSectionFinder() {
+    return {
+      sectionName: this.sectionName,
+      dbId: this.dbId,
+    } as RawSectionFinder<SN, "fe">;
+  }
+  rawSection({
+    sectionName,
+    dbId,
+  }: RawSectionFinder<SN, "fe">): RawSection<SN, "fe"> {
+    const rawSections = this.rawSections[
+      sectionName
+    ] as any as GeneralRawSection[];
+    const rawSection = rawSections.find(
+      (rawSection) => rawSection.dbId === dbId
+    );
+    if (rawSection) return rawSection as any;
+    else throw new Error(`No rawSection found at ${sectionName}.${dbId}`);
+  }
+  get headSection(): RawSection<SN, "fe"> {
+    return this.rawSection(this.headSectionFinder);
+  }
+  changeType<NextSN extends NameToNameWithSameChildren<SN, "fe", "fe">>(
+    nextSectionName: NextSN
+  ): FeSectionPack<NextSN> {
+    const { sectionName } = this;
+    const nextCore = {
+      ...this.core,
+      sectionName: nextSectionName,
+      rawSections: {
+        ...omit(this.rawSections, [sectionName]),
+        [nextSectionName]: this.rawSections[sectionName],
+      },
+    } as any as FeSectionPackCore<NextSN>;
+    return new FeSectionPack(nextCore);
+  }
+  dbToFeIds(
+    childDbIds: SelfOrDescendantIds<SN, "fe">
+  ): SelfOrDescendantIds<SN, "fe"> {
+    return Obj.entries(childDbIds as SelfOrDescendantIds).reduce(
+      (childFeIds, [childName, dbIdArr]) => {
+        (childFeIds[childName] as string[]) = dbIdArr.map(() => Id.make());
+        return childFeIds;
+      },
+      {} as SelfOrDescendantIds
+    ) as SelfOrDescendantIds<SN, "fe">;
+  }
+  makeFeNode(nodeMaker: SectionNodeMaker<SN>): FeSelfOrDescendantNode<SN> {
+    const { dbVarbs, childDbIds } = this.rawSection(
+      nodeMaker as RawSectionFinder<SN, "fe">
+    );
+    return extend(nodeMaker, {
+      dbVarbs,
+      childFeIds: this.dbToFeIds(childDbIds as SelfOrDescendantIds<SN, "fe">),
+    }) as any;
+  }
+  makeParentStub(
+    feNode: FeSelfOrDescendantNode<SN>
+  ): FeSelfOrDescendantParentStub<SN> {
+    const { childDbIds } = this.rawSection(
+      feNode as any as RawSectionFinder<SN, "fe">
+    );
+    return {
+      parentFinder: Inf.fe(feNode.sectionName, feNode.feId),
+      childDbIds,
+      childFeIds: feNode.childFeIds,
+    } as FeSelfOrDescendantParentStub<SN>;
+  }
+  makeHeadNodeMaker({ feId, ...rest }: OrderedSectionNodeProps<SN>) {
+    return {
+      feId: feId ?? Id.make(),
+      dbId: this.dbId,
+      sectionName: this.sectionName,
+      ...rest,
+    } as SectionNodeMaker<SN>;
+  }
+  makeNodeMakers({
+    parentFinder,
+    childDbIds,
+    childFeIds,
+  }: FeSelfOrDescendantParentStub<SN>): SectionNodeMaker<SN>[] {
+    const sectionNodeMakers: SectionNodeMaker<SN>[] = [];
+    for (const [sectionName, dbIds] of Obj.entries(childDbIds)) {
+      const feIds = childFeIds[sectionName];
+      for (const [idx, dbId] of Obj.entries(dbIds)) {
+        sectionNodeMakers.push({
+          dbId,
+          feId: feIds[idx],
+          sectionName,
+          parentFinder: parentFinder,
+        } as OneSectionNodeMaker<SectionName> as SectionNodeMaker<SN>);
+      }
+    }
+    return sectionNodeMakers;
+  }
+  makeOrderedSectionNodes(props: OrderedSectionNodeProps<SN>) {
+    const orderedSectionNodes: FeSelfOrDescendantNode<SN>[] = [];
+    const queue: SectionNodeMaker<SN>[] = [];
+    queue.push(this.makeHeadNodeMaker(props));
+    while (queue.length > 0) {
+      const queueLength = queue.length;
+      for (let i = 0; i < queueLength; i++) {
+        const nodeMaker = queue.shift() as SectionNodeMaker<SN>;
+        const feNode = this.makeFeNode(nodeMaker);
+        orderedSectionNodes.push(feNode);
+        const parentStub = this.makeParentStub(feNode);
+        queue.push(...this.makeNodeMakers(parentStub));
+      }
+    }
+    return orderedSectionNodes;
+  }
+}
--- src/client/src/App/sharedWithServer/Analyzer/SectionPacks/RawSection.ts
+++ src/client/src/App/sharedWithServer/Analyzer/SectionPacks/RawSection.ts
@@ -1,34 +1,34 @@
-import { MergeUnionObjNonNullable } from "../../utils/types/mergeUnionObj";
-// import {
-//   GeneralChildIdArrs,
-//   SelfOrDescendantName,
-// } from "../SectionMetas/relNameArrs/ChildTypes";
-// import { ContextName } from "../SectionMetas/relSections/baseSections";
-// import { SectionName } from "../SectionMetas/SectionName";
-// import { DbVarbs } from "../SectionPack";
-
-// type GeneralRawSection = {
-//   dbId: string;
-//   dbVarbs: DbVarbs;
-//   childDbIds: GeneralChildIdArrs;
-// };
-// export type OneRawSection<
-//   SN extends SectionName,
-//   CN extends ContextName
-// > = GeneralRawSection & {
-//   dbId: string;
-//   dbVarbs: DbVarbs;
-//   childDbIds: OneChildIdArrs<SN, CN>;
-// };
-// export type RawSections<SN extends SectionName, CN extends ContextName> = {
-//   [DSN in SelfOrDescendantName<SN, CN>]: OneRawSection<DSN, CN>[];
-// };
-
-// export type RawSection<
-//   SN extends SectionName,
-//   CN extends ContextName
-// > = RawSections<SN, CN>[SelfOrDescendantName<SN, CN>][number];
-// export type RawSectionWide<
-//   SN extends SectionName,
-//   CN extends ContextName
-// > = MergeUnionObjNonNullable<RawSection<SN, CN>>;
+import {
+  GeneralChildIdArrs,
+  OneChildIdArrs,
+  SelfOrDescendantName,
+} from "../SectionMetas/relNameArrs/ChildTypes";
+import { ContextName } from "../SectionMetas/relSections/baseSections";
+import { DbValue } from "../SectionMetas/relSections/rel/valueMetaTypes";
+import { SectionName } from "../SectionMetas/SectionName";
+export type DbVarbs = {
+  [varbName: string]: DbValue;
+};
+export type GeneralRawSection = {
+  dbId: string;
+  dbVarbs: DbVarbs;
+  childDbIds: GeneralChildIdArrs;
+};
+export type GeneralRawSections = {
+  [key: string]: GeneralRawSection[];
+};
+export type OneRawSection<
+  SN extends SectionName,
+  CN extends ContextName
+> = GeneralRawSection & {
+  dbId: string;
+  dbVarbs: DbVarbs;
+  childDbIds: OneChildIdArrs<SN, CN>;
+};
+export type RawSections<SN extends SectionName, CN extends ContextName> = {
+  [DSN in SelfOrDescendantName<SN, CN>]: OneRawSection<DSN, CN>[];
+};
+export type RawSection<
+  SN extends SectionName,
+  CN extends ContextName
+> = RawSections<SN, CN>[SelfOrDescendantName<SN, CN>][number];
