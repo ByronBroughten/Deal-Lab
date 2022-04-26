49488d8451cd4caa51d62a6da9ad0347f1127907
Byron Broughten <byron.broughten@gmail.com>
2022-04-26 16:42:00 -0500

    Adds tableRow section, propertyTableNext, propertyIndexNext; adds SectionMeta tableSourceName

--- src/client/src/App/sharedWithServer/SectionMetas.ts
+++ src/client/src/App/sharedWithServer/SectionMetas.ts
@@ -195,61 +195,6 @@ export class SectionMetas {
     }
     return selfAndDescendantNames;
   }
-  // Revamp tables
-  // 1. add a "row" section to base and rel sections
-  // 2. make "row" be a child of table, and make each of
-  // the tables have a different row source (everything else)
-  // gets a null row source
-  // 3. Make a rowSource sectionName category at the rel level
-  // 3.1 move all the tableName stuff to sectionMetas
-  // 3.2 make sectionMetas assign tableName to
-  // each of the rowSources (null to the rest)
-  // Use a type to enforce this in sectionMetas
-  // For this, you will need to create a type that shows null
-  // for every section except for the rowSources
-  // 3.3 make a "rowIndexSection" category that takes the index
-  // sections and extracts the ones that are rowSources (I guess)
-  // 4. Fix the functions involved in the Table component
-  // Make them deal just with the table rows rather than index entries
-  // 5. Make make the row index entries lookup use the tables instead
-  // of indexSections
-  // 5.5 Make the rowIndexSections in baseSections and relSections be copies of
-  // what they save.
-  // 6. Make it so that every time a rowIndexSection is saved,
-  // it creates a row on the frontEnd and also saves a table
-  // For this, you will need to create a new route that basically combines.
-  // a sectionArr and section query
-  // 7. Test everything out, I guess!
-
-  // Fix user
-  // 1. Remove the db-only variables from user.
-  // 2. Tack on a custom section to DbUser and MongooseUser
-  // called "serverOnlyUser", with encrypted password, etc
-  // This won't every be queried directly by the client
-
-  // Make the login and register tests pass.
-  // 1. For login, make it save an indexEntry before
-  // logging in, to test that thoroughly
-
-  // Complete the sectionAdder
-  // 1. Make it add a section whereby it uses all default
-  // values and adds only children that are "alwaysOne"
-  // 2. Add the sectionPack loader functionality
-  // 3. Make it able initEmpty or initSectionPack
-  // For this, there are a couple options. You may have it init empty
-  // And then require replacing a section with a sectionPack
-  // You probably don't want to be able to add multiples of the top section
-  // No, I don't think you do.
-
-  // Revamp defaults
-  // 1. Make the FeSections be able to initialize an empty
-  // section whose only children are "alwaysOne"
-  // 2. Make it able to produce rawSectionPacks
-  // 3. Try using it to create default sectionPacks
-  // including perhaps one default analyzer sectionPack (which lacks a loan)
-  // 4. Hook the new sectionAdder up to the main
-
-  // Eliminate the different kinds of SectionPack
 
   private inToOutRelative<
     CN extends ContextName,
--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -9,3 +9,74 @@ AppRegistry.runApplication("App", {
 });
 
 reportWebVitals();
+
+// What should I do about all the current functionality that depends on
+// the way things were? Do I just delete it all right now?
+// It's not going to survive the change in sections.
+// I could have maybe left the sections how they were
+
+// Two options:
+// 1.
+// Revert the changes you made and add new sections for the indexes: propertyIndexNext, etc.
+// Add row
+// Add a new type of table with column and row.
+// Add a new type of propertyIndex, ect.
+// Carry on like that.
+//
+// 2.
+// Destroy all the code for the old indexes
+// I think 1. is going to be the only option.
+
+// Revamp tables
+// 3. Make a rowSource sectionName category at the rel level
+// 3.1 move all the tableName stuff to sectionMetas
+// 3.2 make sectionMetas assign tableName to
+// each of the rowSources (null to the rest)
+// Use a type to enforce this in sectionMetas
+// For this, you will need to create a type that shows null
+// for every section except for the rowSources
+// 3.3 make a "rowIndexSection" category that takes the index
+// sections and extracts the ones that are rowSources (I guess)
+// 4. Fix the functions involved in the Table component
+// Make them deal just with the table rows rather than index entries
+// 5. Make make the row index entries lookup use the tables instead
+// of indexSections
+// 5.5 Make the rowIndexSections in baseSections and relSections be copies of
+// what they save.
+// 6. Make it so that every time a rowIndexSection is saved,
+// it creates a row on the frontEnd and also saves a table
+// For this, you will need to create a new route that basically combines.
+// a sectionArr and section query
+// 7. Test everything out, I guess!
+
+// Fix user
+// 1. Remove the db-only variables from user.
+// 2. Tack on a custom section to DbUser and MongooseUser
+// called "serverOnlyUser", with encrypted password, etc
+// This won't every be queried directly by the client
+
+// Make the login and register tests pass.
+// 1. For login, make it save an indexEntry before
+// logging in, to test that thoroughly
+
+// Complete the sectionAdder
+// 1. Make it add a section whereby it uses all default
+// values and adds only children that are "alwaysOne"
+// 2. Add the sectionPack loader functionality
+// 3. Make it able initEmpty or initSectionPack
+// For this, there are a couple options. You may have it init empty
+// And then require replacing a section with a sectionPack
+// You probably don't want to be able to add multiples of the top section
+// No, I don't think you do.
+
+// Revamp defaults
+// 1. Make the FeSections be able to initialize an empty
+// section whose only children are "alwaysOne"
+// 2. Make it able to produce rawSectionPacks
+// 3. Try using it to create default sectionPacks
+// including perhaps one default analyzer sectionPack (which lacks a loan)
+// 4. Hook the new sectionAdder up to the main
+
+// Get rid of defaultSections and defaultStore.
+// Get rid of different kinds of SectionPack
+// Get rid of dbEntry
