fb6270ef0e78411e0c87a71e74490ba33d8378d9
Byron Broughten <byron.broughten@gmail.com>
2022-05-12 22:19:05 -0500

    Creates first iteration of FullSection class

--- src/client/src/App/sharedWithServer/FeSections/HasSections/FullSection.ts
+++ src/client/src/App/sharedWithServer/FeSections/HasSections/FullSection.ts
@@ -1,38 +1,48 @@
+import { GConstructor } from "../../../utils/classObjects";
 import { FeSectionInfo } from "../../SectionMetas/Info";
 import { ChildName } from "../../SectionMetas/relSectionTypes/ChildTypes";
 import { SectionName } from "../../SectionMetas/SectionName";
-import { DescendantAdderI } from "./DescendantAdder";
-import { SectionAccessor } from "./SectionAccessor";
-import { SectionPackMakerI } from "./SectionPackMaker";
+import { DescendantAdderI, DescendantAdderNext } from "./DescendantAdder";
+import { HasFullSectionProps } from "./HasFullSectionProps";
+import { ApplySectionPackMakers, SectionPackMakerI } from "./SectionPackMaker";
 
 interface FullSectionMixins<SN extends SectionName>
   extends DescendantAdderI<SN>,
     SectionPackMakerI<SN> {}
 
-interface FullSectionI<SN extends SectionName> {
+interface FullSectionI<SN extends SectionName> extends FullSectionMixins<SN> {
   lastChild<CN extends ChildName<SN>>(childName: CN): FullSectionI<CN>;
   child<CN extends ChildName<SN>>(
     childInfo: FeSectionInfo<CN>
   ): FullSectionI<CN>;
 }
 
-function MakeFullSection() {
-  return class FullSection<SN extends SectionName> extends SectionAccessor<SN> {
-    lastChild<CN extends ChildName<SN>>(childName: CN): FullSection<CN> {
-      return this.child(this.childList(childName).last.info);
+function MakeFullSection<
+  SN extends SectionName,
+  TBase extends GConstructor<FullSectionMixins<SN>>
+>(Base: TBase): GConstructor<FullSectionI<SN>> {
+  return class FullSection extends Base implements FullSectionI<SN> {
+    lastChild<CN extends ChildName<SN>>(childName: CN): FullSectionI<CN> {
+      return this.child(this.sections.list(childName).last.info);
     }
     child<CN extends ChildName<SN>>(
       childInfo: FeSectionInfo<CN>
-    ): FullSection<CN> {
+    ): FullSectionI<CN> {
       return this.fullSection(childInfo);
     }
-    private fullSection<IN extends SectionName>(
-      info: FeSectionInfo<IN>
-    ): FullSection<IN> {
+    private get sections() {
+      return this.shared.sections;
+    }
+    private fullSection<S extends SectionName>(
+      info: FeSectionInfo<S>
+    ): FullSectionI<S> {
       return new FullSection({
         shared: this.shared,
         ...info,
-      });
+      } as HasFullSectionProps<SN>) as any as FullSectionI<S>;
     }
   };
 }
+
+const HasSectionPackMakers = ApplySectionPackMakers(DescendantAdderNext);
+const FullSection = MakeFullSection(HasSectionPackMakers);
