903f8576097feb29b60d422bc69a729be5aedb57
Byron Broughten <byron.broughten@gmail.com>
2022-06-24 16:02:24 -0500

    Shifts more away from userServerSide

--- src/routes/apiQueries/register.ts
+++ src/routes/apiQueries/register.ts
@@ -7,26 +7,24 @@ import {
   isRegisterFormData,
 } from "../../client/src/App/sharedWithServer/apiQueriesShared/register";
 import { makeMongooseObjectId } from "../../client/src/App/sharedWithServer/utils/mongoose";
-import { handleResAndMakeError, ResStatusError } from "../../resErrorUtils";
-import { DbSectionsQuerier } from "./shared/DbSections/DbSectionsQuerier";
+import { handleResAndMakeError } from "../../resErrorUtils";
 import { DbUser } from "./shared/DbSections/DbUser";
-import { MakeDbUserProps, userServerSide } from "./userServerSide";
+import { userPrepS } from "./shared/DbSections/DbUser/userPrepS";
+import { userServerSide } from "./userServerSide";
 
 export const registerTestId = makeMongooseObjectId();
 export const nextRegisterWare = [registerServerSide] as const;
 
 async function registerServerSide(req: Request, res: Response) {
   const reqObj = validateRegisterReq(req, res);
-  const { registerFormData, guestAccessSections } = reqObj.body;
-  const { user, serverOnlyUser } = await DbUser.initUserSections(
-    registerFormData
-  );
-  await addUser({
-    user,
-    serverOnlyUser,
-    guestAccessSections,
+  const { registerFormData } = reqObj.body;
+  const { email } = userPrepS.processEmail(registerFormData.email);
+  await userServerSide.entireMakeUserProcess({
+    _id: makeRegisterId(),
+    ...reqObj.body,
   });
-  const dbUser = await DbUser.queryByEmail(user.email);
+
+  const dbUser = await DbUser.queryByEmail(email);
   dbUser.sendLogin(res);
 }
 
@@ -42,31 +40,12 @@ function validateRegisterReq(req: Request, res: Response): NextReq<"register"> {
   if (!areGuestAccessSectionsNext(guestAccessSections)) {
     throw handleResAndMakeError(res, 500, "Invalid guest access sections.");
   }
-
   return makeReq({
     registerFormData,
     guestAccessSections,
   });
 }
 
-async function checkEmailIsUnique(lowercaseEmail: string) {
-  if (await DbSectionsQuerier.existsByEmail(lowercaseEmail)) {
-    throw new ResStatusError({
-      errorMessage: `An account with the email ${lowercaseEmail} already exists.`,
-      resMessage: "An account with that email already exists",
-      status: 400,
-    });
-  }
-}
-async function addUser(makeDbUserProps: MakeDbUserProps) {
-  const _id = makeRegisterId();
-  const { dbUser, mongoUser } = userServerSide.makeDbAndMongoUser({
-    ...makeDbUserProps,
-    _id,
-  });
-  await mongoUser.save();
-  return { ...dbUser, _id };
-}
 function makeRegisterId(): mongoose.Types.ObjectId {
   if (process.env.NODE_ENV === "test") return registerTestId;
   else return makeMongooseObjectId();
--- src/routes/apiQueries/userServerSide.ts
+++ src/routes/apiQueries/userServerSide.ts
@@ -1,43 +1,15 @@
-import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import {
   GuestAccessSectionsNext,
-  RegisterFormData,
   RegisterReqBody,
 } from "../../client/src/App/sharedWithServer/apiQueriesShared/register";
 import { sectionPackS } from "../../client/src/App/sharedWithServer/SectionPack/SectionPack";
-import { BaseSectionsDb } from "../../client/src/App/sharedWithServer/SectionsMeta/baseSectionTypes";
-import { SchemaVarbsToDbValues } from "../../client/src/App/sharedWithServer/SectionsMeta/relSections/rel/valueMetaTypes";
 import { makeMongooseObjectId } from "../../client/src/App/sharedWithServer/utils/mongoose";
 import { DbSectionsModel, UserDbRaw } from "../DbSectionsModel";
 import { serverSectionS } from "../ServerSectionName";
+import { DbUser, UserSections } from "./shared/DbSections/DbUser";
 
 export const userServerSide = {
-  prepEmail(rawEmail: string): PreppedEmails {
-    return {
-      emailAsSubmitted: rawEmail.trim(),
-      get email() {
-        return this.emailAsSubmitted.toLowerCase();
-      },
-    } as const;
-  },
-  async makeUserSections(
-    registerFormData: RegisterFormData
-  ): Promise<UserSections> {
-    const { userName, email: rawEmail, password } = registerFormData;
-    const { email, emailAsSubmitted } = this.prepEmail(rawEmail);
-    return {
-      user: {
-        userName,
-        email,
-        apiAccessStatus: "basicStorage",
-      },
-      serverOnlyUser: {
-        emailAsSubmitted,
-        encryptedPassword: await encryptPassword(password),
-      },
-    };
-  },
   makeDbUser({
     _id = makeMongooseObjectId(),
     user,
@@ -64,14 +36,6 @@ export const userServerSide = {
   makeMongoUser(props: MakeDbUserProps) {
     return new DbSectionsModel(this.makeDbUser(props));
   },
-  makeDbAndMongoUser(props: MakeDbUserProps) {
-    const dbUser = this.makeDbUser(props);
-    const mongoUser = new DbSectionsModel(dbUser);
-    return {
-      dbUser,
-      mongoUser,
-    };
-  },
   async entireMakeUserProcess({
     _id,
     registerFormData,
@@ -79,7 +43,7 @@ export const userServerSide = {
   }: RegisterReqBody & { _id?: mongoose.Types.ObjectId }): Promise<
     UserDbRaw & mongoose.Document<any, UserDbRaw>
   > {
-    const dbSections = await this.makeUserSections(registerFormData);
+    const dbSections = await DbUser.initUserSections(registerFormData);
     const userDoc = this.makeMongoUser({
       ...dbSections,
       guestAccessSections,
@@ -93,24 +57,4 @@ export const userServerSide = {
 export interface MakeDbUserProps extends UserSections {
   _id?: mongoose.Types.ObjectId;
   guestAccessSections: GuestAccessSectionsNext;
-}
-interface UserSections {
-  user: SharedUser;
-  serverOnlyUser: ServerUser;
-}
-
-type SharedUser = SchemaVarbsToDbValues<SharedUserVarbs>;
-type ServerUser = SchemaVarbsToDbValues<ServerOnlyUserVarbs>;
-
-type SharedUserVarbs = BaseSectionsDb["user"]["varbSchemas"];
-type ServerOnlyUserVarbs = BaseSectionsDb["serverOnlyUser"]["varbSchemas"];
-
-type PreppedEmails = {
-  emailAsSubmitted: string;
-  email: string;
-};
-
-async function encryptPassword(unencrypted: string): Promise<string> {
-  const salt = await bcrypt.genSalt(10);
-  return await bcrypt.hash(unencrypted, salt);
 }
