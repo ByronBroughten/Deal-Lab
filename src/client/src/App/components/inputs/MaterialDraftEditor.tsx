905ebbc91077ef89a217f7e4c87204062fee5e05
Byron Broughten <byron.broughten@gmail.com>
2022-10-26 22:13:03 -0500

    Fixes React editor props error

--- src/client/src/App/components/inputs/MaterialDraftEditor.tsx
+++ src/client/src/App/components/inputs/MaterialDraftEditor.tsx
@@ -1,23 +1,15 @@
 import { FilledTextFieldProps, TextField } from "@material-ui/core";
-import { Editor, EditorProps, EditorState } from "draft-js";
+import { Editor, EditorState } from "draft-js";
 import { pick } from "lodash";
 import React from "react";
 import styled from "styled-components";
 import ccs from "../../theme/cssChunks";
 import theme, { ThemeName } from "../../theme/Theme";
 import { MaterialDraftField } from "./MaterialDraftField";
+import { PropAdornments } from "./NumObjEditor/useGetAdornments";
 import { useOnChange } from "./useOnChange";
 
-type ShellProps = {
-  endAdornment?: any;
-  startAdornment?: any;
-};
-interface TweakedEditorProps extends Omit<EditorProps, "onChange"> {
-  editorState: EditorState;
-}
-
 type HandleBeforeInput = (char: string) => "handled" | "not-handled";
-
 interface Props extends Omit<FilledTextFieldProps, "InputProps" | "variant"> {
   sectionName?: ThemeName;
   editorState: EditorState;
@@ -30,12 +22,11 @@ interface Props extends Omit<FilledTextFieldProps, "InputProps" | "variant"> {
   startAdornment?: any;
 }
 
-function getAdornments(shrinkLabel: boolean, shellProps: ShellProps) {
+function getAdornments(shrinkLabel: boolean, shellProps: PropAdornments) {
   if (shrinkLabel) return pick(shellProps, ["endAdornment", "startAdornment"]);
   else return {};
 }
 
-// this can do a memo prop check
 export const MaterialDraftEditor = React.memo(function MaterialDraftEditor({
   id,
   editorState,
@@ -45,6 +36,9 @@ export const MaterialDraftEditor = React.memo(function MaterialDraftEditor({
   label,
   sectionName,
   placeholder,
+  startAdornment,
+  endAdornment,
+  handleBeforeInput,
   ...rest
 }: Props) {
   const editorRef = React.useRef<Editor | null>(null);
@@ -54,7 +48,10 @@ export const MaterialDraftEditor = React.memo(function MaterialDraftEditor({
   const hasText = editorState.getCurrentContent().hasText();
 
   const shrinkLabel = hasFocus || hasText;
-  const { endAdornment, startAdornment } = getAdornments(shrinkLabel, rest);
+  ({ endAdornment, startAdornment } = getAdornments(shrinkLabel, {
+    startAdornment,
+    endAdornment,
+  }));
 
   return (
     <Styled
@@ -79,15 +76,14 @@ export const MaterialDraftEditor = React.memo(function MaterialDraftEditor({
             inputProps: {
               component: Editor,
               editorRef,
-              handleReturn: React.useCallback(() => "handled" as "handled", []),
               editorState,
               placeholder,
               handleOnChange,
+              handleBeforeInput,
+              handleReturn: React.useCallback(() => "handled" as "handled", []),
             } as any,
-            ...{
-              startAdornment,
-              endAdornment,
-            },
+            startAdornment,
+            endAdornment,
           }}
           InputLabelProps={{
             shrink: shrinkLabel,
