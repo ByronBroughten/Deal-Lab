9a5de5f8cdedfdc17de54ac477b77c5f19e6bd21
Byron Broughten <byron.broughten@gmail.com>
2023-10-27 09:10:08 -0500

    Recommends itemizing utilities

--- src/client/src/App/components/ActiveDealPage/ActiveDeal/ActiveDealSections/PropertyEditor/ViewChunks/ViewParts/UtilityValue.tsx
+++ src/client/src/App/components/ActiveDealPage/ActiveDeal/ActiveDealSections/PropertyEditor/ViewChunks/ViewParts/UtilityValue.tsx
@@ -1,3 +1,4 @@
+import { StateValue } from "../../../../../../../sharedWithServer/SectionsMeta/values/StateValue";
 import { DealMode } from "../../../../../../../sharedWithServer/SectionsMeta/values/StateValue/dealMode";
 import { PeriodicMode } from "../../../../../../../sharedWithServer/SectionsMeta/values/StateValue/unionValues";
 import { useGetterSection } from "../../../../../../../sharedWithServer/stateClassHooks/useGetterSection";
@@ -8,28 +9,32 @@ import { MuiSelectItems } from "./../../../../../../appWide/MuiSelect";
 
 function getItems(
   dealMode: DealMode,
-  periodicMode: PeriodicMode
+  periodicMode: PeriodicMode,
+  valueSourceName: StateValue<"utilityValueSource">
 ): MuiSelectItems<"utilityValueSource"> {
+  const itemizeLabel = `Itemize${
+    valueSourceName === "listTotal" ? "" : " (recommended)"
+  }`;
   if (dealMode === "homeBuyer") {
     return [
       ["zero", "Choose method"],
       ["threeHundredPerUnit", "$300 per month"],
       ["valueDollarsPeriodicEditor", "Custom amount"],
-      ["listTotal", "Itemize"],
+      ["listTotal", itemizeLabel],
     ];
   } else if (periodicMode === "holding") {
     return [
       ["zero", "None"],
       ["threeHundredPerUnit", "$300 per unit per month"],
       ["valueDollarsPeriodicEditor", "Custom amount"],
-      ["listTotal", "Itemize"],
+      ["listTotal", itemizeLabel],
     ];
   } else {
     return [
       ["zero", "Tenant pays all utilities"],
       ["threeHundredPerUnit", "Three hundred per unit"],
       ["valueDollarsPeriodicEditor", "Custom amount"],
-      ["listTotal", "Itemize"],
+      ["listTotal", itemizeLabel],
     ];
   }
 }
@@ -56,7 +61,7 @@ export function UtilityValue({ feId, propertyMode, periodicMode }: Props) {
           ...feInfo,
           varbName: "valueSourceName",
         },
-        items: getItems(propertyMode, periodicMode),
+        items: getItems(propertyMode, periodicMode, valueSourceName),
 
         ...(valueSourceName === "valueDollarsPeriodicEditor" && {
           makeEditor: (props) => (
