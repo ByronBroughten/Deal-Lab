ea6f12ccebda47fc9ee8c2034ba309b2b4351461
Byron Broughten <byron.broughten@gmail.com>
2023-11-22 21:17:28 -0600

    Adds PeriodicEditor; switches math cjs to esm

--- src/client/src/App/components/inputs/PeriodicEditor.tsx
+++ src/client/src/App/components/inputs/PeriodicEditor.tsx
@@ -7,14 +7,14 @@ type Props = {
   labelNames: SnVarbNames;
   inputMargins?: boolean;
 };
-export function TimespanEditor({ feId, ...rest }: Props) {
-  const feInfo = { sectionName: "timespanEditor", feId } as const;
+export function PeriodicEditor({ feId, ...rest }: Props) {
+  const feInfo = { sectionName: "periodicEditor", feId } as const;
   const timespanEditor = useGetterSection(feInfo);
   return (
     <NumObjEntityEditor
       {...{
         feVarbInfo: timespanEditor.varbInfo("valueEditor"),
-        endAdornment: ` ${timespanEditor.valueNext("valueEditorUnit")}`,
+        endAdornment: ` ${timespanEditor.valueNext("valueEditorFrequency")}`,
         ...rest,
       }}
     />
